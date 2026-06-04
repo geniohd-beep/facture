28ebc30228a54a0b4efc8dffacd2ffd33ecba8bf
geniohd-beep <geniohd-beep@users.noreply.github.com>
2026-06-04 20:01:09 +0000

    deploy: 30c91ba9015f415f8016aea978a48d80f7efb9f1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3916524541" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4114495965" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
