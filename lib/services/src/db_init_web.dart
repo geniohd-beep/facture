61a8fd6e8b8d1209e84f74794328c6ac2a162e33
dan <henryparedes.m@gmail.com>
2026-06-04 18:04:27 -0500

    fix: use no-web-worker SQLite mode for Firefox compat

--- lib/services/src/db_init_web.dart
+++ lib/services/src/db_init_web.dart
@@ -2,5 +2,5 @@ import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'package:sqflite/sqflite.dart' show databaseFactory;
 
 void initDatabaseFactory() {
-  databaseFactory = databaseFactoryFfiWeb;
+  databaseFactory = databaseFactoryFfiWebNoWebWorker;
 }
